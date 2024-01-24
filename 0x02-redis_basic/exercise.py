8337140056a9de847dc9b8bae307c9a742cf69b7
joekariuki3 <joekariuki3@gmail.com>
2024-01-24 14:38:43 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Any
 
 
 class Cache:
@@ -16,7 +16,27 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """takes data stores it in resid and returns its id as Key"""
+        """takes data stores it in redis and returns its id as Key"""
         newKey = str(uuid.uuid4())
         self._redis.set(newKey, data)
         return newKey
+
+    def get(self, key: str, fn: Callable) -> Union[str, int, None]:
+        """retrives a value for the passed key
+        then uses fn to convert the value to its appropriate
+        type return the value if found or None"""
+        if not key:
+            return None
+        binaryData = self._redis.get(key)
+        if not binaryData:
+            return None
+        data = fn(binaryData)
+        return data
+
+    def get_str(self, binaryData: bytes) -> str:
+        """converts binaryData to string and returns it"""
+        return binaryData.decode('utf-8')
+
+    def get_int(self, binaryData: bytes) -> int:
+        """converts binaryData to int and returns it"""
+        return int(binaryData)
